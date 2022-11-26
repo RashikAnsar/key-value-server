5229ffbfb961c7b1f865b6cb1421188ff1b8dba9
RashikAnsar <sk.ansar46@gmail.com>
2022-11-26 12:15:03 +0530

    feat: basic functionalities of key-value store

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +11,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
+
+// In memory key-value store
+var data = map[string]string{}
 
 func main() {
 	port := "8080"
@@ -25,6 +30,45 @@ func main() {
 		JSON(w, map[string]string{"hello": "world"})
 	})
 
+	r.Get("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+		data, err := Get(r.Context(), key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+		w.Write([]byte(data))
+	})
+
+	r.Delete("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+		err := Delete(r.Context(), key)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+		JSON(w, map[string]string{"status": "success"})
+	})
+
+	r.Post("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+		err = Set(r.Context(), key, string(body))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+		}
+		JSON(w, map[string]string{"status": "success"})
+	})
+
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
@@ -37,4 +81,18 @@ func JSON(w http.ResponseWriter, data interface{}) {
 		return
 	}
 	w.Write(b)
+}
+
+func Set(ctx context.Context, key, value string) error {
+	data[key] = value
+	return nil
+}
+
+func Get(ctx context.Context, key string) (string, error) {
+	return data[key], nil
+}
+
+func Delete(ctx context.Context, key string) error {
+	delete(data, key)
+	return nil
 }
