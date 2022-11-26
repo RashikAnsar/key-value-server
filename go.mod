cffe7252a85e3b7fbce341824debddb7f0d34da5
RashikAnsar <sk.ansar46@gmail.com>
2022-11-26 11:29:14 +0530

    refactor: use chi server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module key-value-store
 
 go 1.19
+
+require github.com/go-chi/chi v1.5.4
--- server.go
+++ server.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World\n")
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	log.Printf("starting up on http://localhost:%s", port)
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	// // Custom middleware for logging
+	// r.Use(func(next http.Handler) http.Handler {
+	// 	fn := func(w http.ResponseWriter, r *http.Request) {
+	// 		log.Printf("got request: %+v\n", r)
+	// 		next.ServeHTTP(w, r)
+	// 	}
+	// 	return http.HandlerFunc(fn)
+	// })
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		JSON(w, map[string]string{"hello": "world"})
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func JSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	b, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		JSON(w, map[string]string{"error": err.Error()})
+		return
+	}
+	w.Write(b)
 }
